642a32882cfdc39cfb761ec9e33e87f54d5c4e8c
IndraRusyana <indrarusyana@gmail.com>
2025-08-15 21:29:54 +0700

    fix lgi2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'),
+        login: path.resolve(__dirname, 'src/login.html')
+      }
+    }
   },
   server: {
     open: '/login.html'
